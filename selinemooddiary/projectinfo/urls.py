7de2de9bbb215df2f74b6ab3085b9ee42b0428df
Paulin <battlemavlin@yandex.ru>
2024-10-28 12:04:18 +0300

    aboutproject basic views
    Добавил html-шаблон для страницы "О проекте", вывел её в /about. Заодно обновил requirements.

--- selinemooddiary/projectinfo/urls.py
+++ selinemooddiary/projectinfo/urls.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.urls import path, include
+from .views import index
 
 urlpatterns = [
-    path('', lambda request: HttpResponse('200')),
+    path('', index),
 ]
