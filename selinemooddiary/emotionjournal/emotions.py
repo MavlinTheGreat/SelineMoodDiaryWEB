6d88448813500280cc2d8ae55410e31cbf4f01dc
Pavel Nikitin <pavlnik1@yandex.ru>
2024-12-12 01:07:11 +0300

    Emotion journal to REST Framework + minor fixes
    i really don't know is it working or not is it just code or functionality i am so sleepy and so dumb i just want to fall asleep for month i am so tired just let me get out of there but i need to work for all those expectations goals mission I CANNOT I AM REALLY TIRED EVEN IF I AM DOING NOTHING all my energy sources or drained or inactive that is hell on the earth that is becoming closer and closet to my skin giving me just a little chills but in fact threating a knife near my throat that's all so empty so dumb so senseless just WHY AM I DOING IT WHY AM I ALIVE WHY I DIDN'T DIE THAT DAY? Just for a smile. just for one smile that is fading away from my mind further and further because i am drowning in absolute panic at the gates of this frightning world and nobody nobody nobody nobody can really help me except for MYSELF MYSELF WHO ALREADY DROWNED ON SO MANY LEVELS
    whooooh, well, carry on

--- selinemooddiary/emotionjournal/emotions.py
+++ selinemooddiary/emotionjournal/emotions.py
@@ -8,11 +8,15 @@ class Emotion(models.Model):
     name = models.CharField(max_length=64) # название эмоции
     description = models.TextField(null=True) # её описание, синонимы и т.д.
     rating = models.PositiveSmallIntegerField( # численная оценка настроения, стоящая за иконкой
-        validators=[MinValueValidator(1), MaxValueValidator(10)]) # ограничивает значения от 1 до 10
-    imageIcon = models.FileField(null=True)
-    owner = models.ForeignKey(DiaryUser, on_delete=models.CASCADE, default=None, null=True)# кому принадлежит эмоция
+    validators=[MinValueValidator(1), MaxValueValidator(10)]) # ограничивает значения от 1 до 10
+    imageIcon = models.ImageField(null=True, upload_to="images/emotionicons/")
+    owner = models.ForeignKey(DiaryUser, on_delete=models.CASCADE, default=None, null=True) # кому принадлежит эмоция
     # None -> доступно всем
 
     def __str__(self):
         return self.name
 
+    @property
+    def is_global(self):
+        """Проверка, является ли эмоция глобальной."""
+        return self.owner is None
--- selinemooddiary/selinemooddiary/settings.py
+++ selinemooddiary/selinemooddiary/settings.py
@@ -3,10 +3,13 @@ from datetime import timedelta
 
 from pathlib import Path
 from decouple import config
-
+from django.conf.global_settings import MEDIA_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 SECRET_KEY = config("SECRET_KEY")
